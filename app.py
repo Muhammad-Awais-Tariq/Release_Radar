89c05d45ddfe01b49f3aae95783b958b9368afd9
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-14 18:59:50 +0500

    added the genres list

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ load_dotenv()
 
 N8N_WEBHOOK_URL = os.getenv("N8N_WEBHOOK_URL")
 
+st.title("Release Radar")
+
+genres = ["Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary",
+          "Drama", "Family", "Fantasy", "History", "Horror", "Music", "Mystery",
+          "Romance", "Sci-Fi", "Thriller", "War", "Western"]
+
+genre = st.selectbox("Pick a genre" , genres)
+
